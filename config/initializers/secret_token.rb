2ddd26027efdc1777437b445a258ea0eb9eb12b4
radamanthus <rad@batnag.org>
2014-02-15 06:24:32 +0800

    Added empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsWorkshopApp::Application.config.secret_key_base = 'e5fbb36fcacb90a86fccaf2e251e28775e71f44442e378a7cf611b38facb5fa74a940ef691d4af6576cced859b4c5d8e7ddcd4a4bc8fff7ade5ce91979e3467a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsWorkshopApp::Application.config.session_store :cookie_store, key: '_rails-workshop-app_session'
